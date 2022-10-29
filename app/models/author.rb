560f521800f21c067be311d57be76090f19f8c7c
ruthwekesa <wekesaruth569@gmail.com>
2022-10-29 21:57:26 +0300

    completed challenge

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,33 @@
+require_relative './module'
 class Author
-  attr_accessor :name
-
-
+  attr_reader :name
+  extend Finder
+  
+  @@all=[]
   def initialize(name)
     @name = name
+    @@all<<self
+  end
   
+  def self.all
+    @@all
   end
 
+  def articles
+    Article.all.filter{|article|article.author==@name}
+  end
+
+  def magazines
+    articles.map{|article|article.magazine}.uniq
+  end
+
+  def add_article(magazine, title)
+    Article.new(self,magazine,title)
+  end
+  
+  def topic_areas
+    magazines.map{|magazine|magazine.category}.uniq
+  end
 
 end
+
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,35 @@
+require_relative './module'
 class Magazine
   attr_accessor :name, :category
-
-
+  extend Finder
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  def magazine_articles
+    Article.all.filter{|article|article.magazine.name==@name}
+  end
+  def magazine_authors
+    magazine_articles.map{|article|Author.find_by_name(article.author)}
+  end
+  
+  def contributors
+    magazine_authors.uniq
+  end
+  
+  def article_titles
+    magazine_articles.map{|article|article.title}
+  end
 
+  def contributing_authors
+    main_authors=[]
+    magazine_authors.tally.each{|key,value|value>2 && (main_authors<<key)}
+    main_authors
+  end
 end
